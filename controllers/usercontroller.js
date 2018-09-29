0af13c79843dab2374df4ff176abda2c49f0ad9d
Benjamin Donahue <benjamindonahue@Benjamins-MacBook-Pro.local>
2018-09-29 18:43:39 -0400

    after 7

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,6 +2,7 @@ let express = require('express')
 let router = express.Router()
 let sequelize = require('../db');
 let User = sequelize.import('../models/user');
+let bcrypt = require('bcryptjs');
 let jwt = require('jsonwebtoken');
 
 /*************************
@@ -15,10 +16,14 @@ router.post('/createuser', function (req, res) {
 
     User.create({
         username: username,
-        passwordhash: pass
+        passwordhash: bcrypt.hashSync(pass, 10)
     }).then(
         function createsuccess(user) {
-            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+            let token = jwt.sign({
+                id: user.id
+            }, process.env.JWT_SECRET, {
+                expiresIn: 60 * 60 * 24
+            });
 
             res.json({
                 user: user,
@@ -30,6 +35,41 @@ router.post('/createuser', function (req, res) {
             res.send(500, err.merssage)
         }
     )
+})
+
+router.post('/signin', function (req, res) {
+    User.findOne({
+            where: {
+                username: req.body.user.username
+            }
+        })
+        .then(
+            function (user) {
+                if (user) {
+                    bcrypt.compare(req.body.user.password, user.passwordhash, function (err, matches) {
+                        if(matches) {
+                            let token = jwt.sign({ id: user.id}, process.env.JWT_SECRET, {expiresIn:60*60*24});
+                            res.json({
+                                user: user,
+                                message: "SUCCESSFULLY AUTHENTICATED",
+                                sessionToken: token
+                            })
+                        } else {
+                            res.status(502).send({ error: "YOU FAILED, YO! BOO!!! (502)"})
+                        }
+                    })
+                } else {
+                    res.status(500).send({
+                        error: "FAILED TO AUTHENTICATE"
+                    })
+                }
+            },
+            function (err) {
+                res.status(501).send({
+                    error: "YOU FAILED, YO!!! (501)"
+                })
+            }
+        )
 })
 
 module.exports = router
