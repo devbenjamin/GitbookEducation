ff4e4acc46c9c539b604d18de172bfc73fe433eb
Benjamin Donahue <benjamindonahue@Benjamins-MacBook-Pro.local>
2018-09-29 14:54:19 -0400

    after 6.3.3

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -5,29 +5,31 @@ let User = sequelize.import('../models/user');
 let jwt = require('jsonwebtoken');
 
 /*************************
-** Create User Endpoint: Starter***
-**************************/
+ ** Create User Endpoint: Starter***
+ **************************/
 
 router.post('/createuser', function (req, res) {
 
-    var username = "req.body.user.username;";
-    let pass = "req.body.user.password";
+    let username = req.body.user.username;;
+    let pass = req.body.user.password;
 
     User.create({
         username: username,
         passwordhash: pass
-    }) .then(
-        function message(user){
+    }).then(
+        function createsuccess(user) {
+            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+
             res.json({
-            user: user,
-            message: 'created'
-        })
-    },
-    function createError(err) {
-        res.send(500, err.merssage)
-    }
+                user: user,
+                message: 'created',
+                sessionToken: token
+            })
+        },
+        function createError(err) {
+            res.send(500, err.merssage)
+        }
     )
 })
 
 module.exports = router
-
